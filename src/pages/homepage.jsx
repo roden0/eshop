b3ad0b9c6c6cca09ccb745c17707b1335d78c833
Rodrigo Encinas <roden7@hotmail.com>
2020-07-23 23:02:06 +0200

    added components

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.scss';
+
+import Mosaic from '../components/mosaic/mosaic';
+
+const HomePage = () => (
+    <div className="page homepage">
+        <Mosaic />
+    </div>
+);
+
+export default HomePage;
