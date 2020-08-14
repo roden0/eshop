474bdc042ac2b918514ccdfb00505fccb4b24c31
Rodrigo Encinas <roden7@hotmail.com>
2020-08-14 15:29:52 +0200

    added payment server

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({amount}) => {
     const amountForStripe = amount * 100;
     const pubKey = "pk_test_51HBiQSLwoiP51HYTEqPHsYvdGKbJJjlX81aAAQO3p6zOGNBtORpTmlsuiD9RmFZuspUmPBlRzUAGVj33v9TkBESr00BF7QvAId";
 
     const onToken = token =>{
-        console.log(token);
+        axios({
+            url: 'https://seres-server.herokuapp.com/payment',
+            method: 'post',
+            data: {
+                amount: amountForStripe,
+                token
+            }
+        })
+        .then(res=> {
+            alert('Payment Successful');
+        })
+        .catch(err =>{
+            console.error('Payment Error: '+JSON.parse(err));
+            alert('Payment Error. Check your data');
+        });
     }
 
     return(
